b6f73aa8cf3d478a7dc54f3abeafd2afc69ff82f
Vaibhav <vaibhavsinghvrajpot@gmail.com>
2023-09-30 20:41:22 +0530

    start

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Expense from './components/Expense';
+
+function App() {
+  return (
+   <>
+   <Expense />
+   </>
+  );
+}
+
+export default App;
---
+++ src/components/Expense.js
@@ -0,0 +1,13 @@
+
+function Expense(){
+   return(
+    <>
+  <h2> Expense Items </h2>
+  <p> Food Rs. {10} </p>
+  <p> Petrol Rs. {100} </p>
+  <p> Movie Rs. {200} </p>
+    </>
+   )
+}
+
+export default Expense ;
