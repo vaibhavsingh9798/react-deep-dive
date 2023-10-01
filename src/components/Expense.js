c615a303deab44a60a343113d5b81fed7f8f307e
Vaibhav <vaibhavsinghvrajpot@gmail.com>
2023-10-01 10:55:15 +0530

    partially dynamic

--- src/components/Expense.js
+++ src/components/Expense.js
@@ -1,12 +1,21 @@
-
+import './Expense.css'
 function Expense(){
+
+    let items = [{name:"Food",price:200},{name:"Movie",price:300},{name:"Petrol",price:400}]
    return(
     <>
-  <h2> Expense Items </h2>
-  <p> Food Rs. {10} </p>
-  <p> Petrol Rs. {100} </p>
-  <p> Movie Rs. {200} </p>
-    </>
+       <div className="expenses-container">
+      <h1>Expenses List</h1>
+      <ul className="expenses-list">
+        {items.map((expense, index) => (
+          <li key={index} className="expense-item">
+            <div className="expense-name">{expense.name}</div>
+            <div className="expense-amount">{expense.price}</div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  </>
    )
 }
 
