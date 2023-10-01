39532a7355a74d5cccde2788baa27981d1cd1923
Vaibhav <vaibhavsinghvrajpot@gmail.com>
2023-10-01 12:01:02 +0530

    props added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 
 import './App.css';
 import Expense from './components/Expense';
-
+ 
 function App() {
+  let items = [{name:"Food",price:200},{name:"Movie",price:300},{name:"Petrol",price:400}]
   return (
    <>
-   <Expense />
+   <Expense items={items}/>
    </>
   );
 }
--- src/components/Expense.js
+++ src/components/Expense.js
@@ -1,13 +1,11 @@
 import './Expense.css'
-function Expense(){
-
-    let items = [{name:"Food",price:200},{name:"Movie",price:300},{name:"Petrol",price:400}]
+function Expense(props){ 
    return(
     <>
        <div className="expenses-container">
       <h1>Expenses List</h1>
       <ul className="expenses-list">
-        {items.map((expense, index) => (
+        {props.items.map((expense, index) => (
           <li key={index} className="expense-item">
             <div className="expense-name">{expense.name}</div>
             <div className="expense-amount">{expense.price}</div>
